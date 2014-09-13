ec41f8d746e9c27ded3e217e2466538fb0ba6de7
1005911753@qq.com <1005911753@qq.com>
2014-09-13 10:51:57 +0800

    初始版本

---
+++ Tieba_Recent_Emoi.meta.js
@@ -0,0 +1,16 @@
+﻿// ==UserScript==
+// @name        Tieba_Recent_Emoi
+// @namespace   Tieba_Recent_Emoi
+// @description 贴吧最近使用表情
+// @include     http://tieba.baidu.com/*
+// @version     1.1
+// @require http://static.hdslb.com/js/jquery.min.js
+// @updateURL   https://nightlyfantasy.github.io/Tieba_Recent_Emoi/Tieba_Recent_Emoi.meta.js
+// @downloadURL https://nightlyfantasy.github.io/Tieba_Recent_Emoi/Tieba_Recent_Emoi.user.js
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_addStyle
+// @grant		GM_deleteValue
+// @grant       unsafeWindow
+// @author     绯色
+// ==/UserScript==
